519881d184f72ce95d32637602fe710bdaa835e5
jelenamiletic <jelenamiletic215@gmail.com>
2021-11-19 15:37:38 +0100

    Security conf changed so it doesn't interfere with other work

--- Sigma/src/main/java/com/kts/sigma/security/WebSecurityConfig.java
+++ Sigma/src/main/java/com/kts/sigma/security/WebSecurityConfig.java
@@ -48,17 +48,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-//        // We don't need CSRF for this example
-//        httpSecurity.csrf().disable()
-//                // dont authenticate this particular request
-//                .authorizeRequests().antMatchers("/authenticate").permitAll().
-//                // all other requests need to be authenticated
-//                        anyRequest().anonymous().and().
-//                // make sure we use stateless session; session won't be used to
-//                // store user's state.
-//                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //TODO treba da se proveri koje putanje cemo dozvoliti samo menadzeru..
+        // posto nemamo autorizaciju za ostale role u sistemu.. moracemo tako
+//        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        // We don't need CSRF for this example
+        httpSecurity.csrf().disable()
+                // dont authenticate this particular request
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                // all other requests need to be authenticated
+                        anyRequest().permitAll().and().
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
+                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
