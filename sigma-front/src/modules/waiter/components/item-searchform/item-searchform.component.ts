ecaf0b4497178378bc3e6f026273fd99b84f1c2d
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-01-22 12:35:03 +0100

    Search fix

--- sigma-front/src/modules/waiter/components/item-searchform/item-searchform.component.ts
+++ sigma-front/src/modules/waiter/components/item-searchform/item-searchform.component.ts
@@ -1,92 +1,98 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { MatTableDataSource } from "@angular/material/table";
-import { Item } from "src/modules/root/models/item";
-import { Menu } from "src/modules/root/models/menu";
-import { Table } from "src/modules/root/models/table";
-import { WaiterOrderService } from "../../services/waiter-order.service";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Item } from 'src/modules/root/models/item';
+import { Menu } from 'src/modules/root/models/menu';
+import { WaiterOrderService } from '../../services/waiter-order.service';
 
 @Component({
-    selector: 'app-item-searchform',
-    templateUrl: './item-searchform.component.html',
-    styleUrls: ['./item-searchform.component.scss'],
+  selector: 'app-item-searchform',
+  templateUrl: './item-searchform.component.html',
+  styleUrls: ['./item-searchform.component.scss'],
 })
 export class ItemSearchformComponent implements OnInit {
-    @Output() addItemToOrder: EventEmitter<any> = new EventEmitter();
-    items: Array<Item>;
-    activeNonExpiredMenus: Array<Menu>;
-    selectedMenu: Menu;
-    originalItemData: Array<Item>;
-    searchForm!: FormGroup;
-    foodIsTrue: boolean;
-    isLoading: boolean;
-    categories: Array<string>;
-    types: Array<string>;
+  @Output() addItemToOrder: EventEmitter<any> = new EventEmitter();
+  items: Array<Item>;
+  activeNonExpiredMenus: Array<Menu>;
+  selectedMenu: Menu;
+  originalItemData: Array<Item>;
+  searchForm!: FormGroup;
+  categories: Array<string>;
 
+  constructor(private waiterOrderService: WaiterOrderService) {
+    this.items = [];
+    this.activeNonExpiredMenus = [];
+    this.selectedMenu = new Menu();
+    this.categories = ['ALL', 'APPETISER', 'SALAD', 'MAIN_COURSE', 'DESERT'];
+    this.originalItemData = [];
+  }
 
-    constructor(
-        private foodDrinksService: WaiterOrderService,
-    ) {
-        this.items = [];
-        this.activeNonExpiredMenus = [];
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      searchTerm: new FormControl(''),
+    });
+    this.getActiveNonExpiredMenus();
+  }
+
+  getActiveNonExpiredMenus(): void {
+    this.waiterOrderService.getActiveNonExpiredMenus().subscribe((data) => {
+      this.activeNonExpiredMenus = data;
+      if (data.length !== 0) {
+        this.selectedMenu = data[0];
+        this.getAllItems();
+      } else {
         this.selectedMenu = new Menu();
-        this.isLoading = false;
-        this.categories = ['ALL', 'APPETISER', 'SALAD', 'MAIN_COURSE', 'DESERT'];
-        this.types = ['APPETISER', 'SALAD', 'MAIN_COURSE', 'DESERT'];
-        this.originalItemData = [];
-        this.foodIsTrue = true;
-    }
+      }
+    });
+  }
 
-    ngOnInit(): void {
-        this.searchForm = new FormGroup({
-            searchTerm: new FormControl(''),
+  changeMenu(menuId: number): void {
+    this.selectedMenu = this.activeNonExpiredMenus.filter(
+      (m) => m.id === menuId
+    )[0];
+    this.waiterOrderService
+      .getAllItems(this.selectedMenu.id)
+      .subscribe((data) => {
+        this.items = data;
+        this.originalItemData = data;
+      });
+  }
+
+  getAllItems(): void {
+    this.waiterOrderService
+      .getAllItems(this.selectedMenu.id)
+      .subscribe((data) => {
+        this.items = data;
+        this.originalItemData = data;
+      });
+  }
+
+  filterByCategory(category: string): void {
+    if (category === 'ALL') {
+      this.items = this.originalItemData;
+    } else {
+      this.waiterOrderService
+        .getItemsByFoodType(this.selectedMenu.id, category)
+        .subscribe((data) => {
+          this.items = data;
         });
-        this.getActiveNonExpiredMenus();
     }
+  }
 
-    getActiveNonExpiredMenus(): void {
-        this.foodDrinksService.getActiveNonExpiredMenus().subscribe((data) => {
-            this.activeNonExpiredMenus = data;
-            if (data.length !== 0) {
-                this.selectedMenu = data[0];
-                this.getAllItems();
-            } else {
-                this.selectedMenu = new Menu();
-            }
+  search(): void {
+    if (this.searchForm.get('searchTerm')?.value !== '') {
+      this.waiterOrderService
+        .getItemsBySearchTerm(
+          this.selectedMenu.id,
+          this.searchForm.get('searchTerm')?.value
+        )
+        .subscribe((data) => {
+          this.items = data;
         });
     }
+  }
 
-    getAllItems(): void {
-        this.foodDrinksService.getAllItems(this.selectedMenu.id).subscribe((data) => {
-            this.items = data;
-            this.originalItemData = data;
-        });
-    }
-
-    filterByCategory(category: string): void {
-        if (category === 'ALL') {
-            this.items = this.originalItemData;
-        } else {
-            this.foodDrinksService.getItemsByFoodType(this.selectedMenu.id, category).subscribe((data) => {
-                this.items = data;
-            });
-        }
-    }
-
-    search(): void {
-        if (this.searchForm.get('searchTerm')?.value !== '') {
-            this.foodDrinksService
-                .getItemsBySearchTerm(this.selectedMenu.id, this.searchForm.get('searchTerm')?.value)
-                .subscribe((data) => {
-                    this.items = data;
-                });
-        }
-    }
-
-    resetSearchFilter(): void {
-        this.items = this.originalItemData;
-        this.searchForm.patchValue({ searchTerm: '' });
-    }
-
-
+  resetSearchFilter(): void {
+    this.items = this.originalItemData;
+    this.searchForm.patchValue({ searchTerm: '' });
+  }
 }
