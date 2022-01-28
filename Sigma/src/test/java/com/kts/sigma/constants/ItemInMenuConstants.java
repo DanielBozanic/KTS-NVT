d71046e74880a3be79063ba25207ed87cfa5d7d6
jelenamiletic <jelenamiletic215@gmail.com>
2022-01-28 12:46:18 +0100

    ItemInMenu Integration Test fixed

--- Sigma/src/test/java/com/kts/sigma/constants/ItemInMenuConstants.java
+++ Sigma/src/test/java/com/kts/sigma/constants/ItemInMenuConstants.java
@@ -1,5 +1,7 @@
 package com.kts.sigma.constants;
 
+import com.kts.sigma.model.FoodType;
+
 public class ItemInMenuConstants {
 	
 	public final static Integer DB_ITEM_IN_MENU_ID_1 = 1;
@@ -9,6 +11,8 @@ public class ItemInMenuConstants {
 
 	public final static Integer INVALID_ITEM_ID = -100;
 	public final static Integer DB_DELETE_ITEM_ID = 1;
+	public final static Integer VALID_MENU_ID = 1;
+	public final static Integer INVALID_MENU_ID = 3;
 	
 	public final static Integer NEW_ITEM_IN_MENU_ID = 5;
 	
@@ -21,4 +25,10 @@ public class ItemInMenuConstants {
 	public final static Integer TOTAL_ACTIVE_ITEMS_IN_SECOND_MENU = 1;
 	public final static Integer TOTAL_ACTIVE_ITEMS_IN_MENU = 3;
 	public final static Integer TOTAL_ITEMS_IN_MENU = 4;
+
+	public final static String VALID_SEARCH_TERM= "Pizza";
+	public final static String INVALID_SEARCH_TERM= "Invalid_food";
+
+	public final static FoodType VALID_FOOD_TYPE= FoodType.MAIN_COURSE;
+
 }
